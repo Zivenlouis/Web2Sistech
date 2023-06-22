a161cf22b5565c7256314b0c5e20dcfdf15ed79e
jasontjoa1 <tjoajason2003@gmail.com>
2023-06-22 23:45:43 +0700

    tambah

--- User/news.php
+++ User/news.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>News</title>
+    <<?php require_once("Layout/head.php");?>
 </head>
 <body>
     
