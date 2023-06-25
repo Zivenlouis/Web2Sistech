0474bce04e93905c3f9d1b11ee9b44101ff213b7
Irwanto Wijaya <irwantowijaya0@gmail.com>
2023-06-25 11:07:10 +0700

    a

--- Admin/editProfile.php
+++ Admin/editProfile.php
@@ -1,3 +1,35 @@
+<?php
+require_once("php/connection.php");
+
+if(isset($_POST['submit'])){
+  $id = $_POST['id'];
+  $title = $_POST['title'];
+  $description = $_POST['description']; 
+  $submittedImage = $_FILES['image']['tmp_name'];
+  $imageName = uniqid() . '.' . pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+  $uploadFolder = '../UploadImage/Profile';
+  $destination = $uploadFolder . '/' . $imageName;
+  move_uploaded_file($submittedImage, $destination);
+  date_default_timezone_set('Asia/Jakarta');
+  $time = date("Y-m-d H:i:s");
+  $sql = "UPDATE tbl_admin_profile SET profile_title='$title', profile_description='$description', profile_image='$imageName', last_modified='$time' WHERE profile_id='$id'";
+  $result = $conn->query($sql);
+  if ($result) {
+    header("Location: profile.php");
+    exit();
+  } else {
+    echo "Update failed. Please try again.";
+  }
+}
+  $sql = "SELECT * FROM tbl_admin_profile";
+  $result = $conn->query($sql);
+  $row = $result->fetch_assoc();
+  $profileTitle = $row['profile_title'];
+  $profileDescription = $row['profile_description'];
+  $profileImage = $row['profile_image'];
+  $id = $row['profile_id'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,15 +46,6 @@
             <p class="card-description"> Edit Profile Data </p>
             <form class="forms-sample" method="post" enctype="multipart/form-data">
               <div class="form-group row">
-                <?php
-                    $sql = "SELECT * FROM tbl_admin_profile";
-                    $result = $conn->query($sql);
-                    $row = $result->fetch_assoc();
-                    $profileTitle = $row['profile_title'];
-                    $profileDescription = $row['profile_description'];
-                    $profileImage = $row['profile_image'];
-                    $id = $row['profile_id'];
-                ?>
                 <label for="eventTitle" class="col-sm-3 col-form-label">Profile Title</label>
                 <div class="col-sm-9">
                   <input type="text" name="title" value="<?php echo $profileTitle; ?>" class="form-control" id="profileTitle" placeholder="Title" style="color: #ffff">
@@ -42,29 +65,8 @@
               </div>
               <input type="hidden" name="id" value="<?php echo $id; ?>">
               <button type="submit" class="btn btn-primary mr-2" name="submit">Update</button>
-              <a href="profile.php" class="btn btn-dark">Cancel</a>
+              <a href="profile.php" class="btn btn-dark">Back</a>
             </form>
-            <?php
-            require_once("php/connection.php");
-
-            if(isset($_POST['submit'])){
-                $id = $_POST['id'];
-                $title = $_POST['title'];
-                $description = $_POST['description']; 
-                if(isset($_FILES['image']['tmp_name']) && !empty($_FILES['image']['tmp_name'])){
-                    $submittedImage = $_FILES['image']['tmp_name'];
-                    $imageName = uniqid() . '.' . pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
-                    $uploadFolder = '../UploadImage/Profile';
-                    $destination = $uploadFolder . '/' . $imageName;
-                    move_uploaded_file($submittedImage, $destination);
-                    date_default_timezone_set('Asia/Jakarta');
-                    $time = date("Y-m-d H:i:s");
-                    $sql = "UPDATE tbl_admin_profile SET profile_title='$title', profile_description='$description', profile_image='$imageName', last_modified='$time' WHERE profile_id='$id'";
-                    $result = $conn->query($sql);
-                    header("location:profile.php");
-                }
-            } 
-            ?>
           </div>
         </div>
       </div>
