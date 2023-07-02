aa14ef55e754d4efcfabb080f6af4896f96d0f1e
Zivenlouis <zivenlouisuph@gmail.com>
2023-07-02 10:36:56 +0700

    oke

--- Admin/php/connection.php
+++ Admin/php/connection.php
@@ -1,9 +1,3 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";  
-    $database = "db_web2";
-    $port = "3306";
-    $conn = new mysqli($servername, $username, $password, $database, $port);
-
+    require("../connection.php");
 ?>    
