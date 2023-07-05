f42ebbe569bdc5bb7a69b678ca6da9e7c49c98f8
Zivenlouis <zivenlouisuph@gmail.com>
2023-07-05 20:14:38 +0700

    finalisasi

--- db_web2.sql
+++ db_web2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Jul 04, 2023 at 12:54 PM
+-- Generation Time: Jul 05, 2023 at 03:12 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -74,10 +74,10 @@ CREATE TABLE `tbl_admin_events` (
 --
 
 INSERT INTO `tbl_admin_events` (`id`, `event_title`, `event_description`, `price`, `active`, `event_long_image`, `event_square_image`, `time_created`, `last_modified`) VALUES
-(6, 'Bangkit 2023', 'Aim Higher With Bangkit 2023 merupakan program kerja oleh Himpunan Mahasiswa Program Studi Sistem Informasi (HMPSSI) dan Himpunan Mahasiswa Program Studi Informatika (HMPTIF) UPH Kampus Medan yang bertujuan untuk memperkenalkan program MBKM terkhususnya Bangkit untuk mahasiswa/i UPH Kampus Medan...\r<br>\r<br>', 25000, '1', '649d3d458f960.png', '649d3d458f7a6.png', '2023-06-29 15:13:57', '2023-07-01 17:04:38'),
-(7, 'Menara: Metaverse in Nusantara', 'Menara: Metaverse in Nusantara is an event held by SISTECH to introduce and enrich participants\' knowledge about the technology behind Metaverse and the development of Metaverse in Indonesia...', 35000, '0', '649d3d5a9308c.png', '649d3d5a92ee9.png', '2023-06-29 15:14:18', '2023-06-29 15:15:02'),
-(8, 'Basic Coding Class', 'Techno has been eavesdropping, hearing rumors that some of you are confused on where to start your first coding journey. Therefore, we’re bringing back “Basic Coding Class” as a platform where you can learn about the fundamentals of coding and sharpen your logical skills...', 125000, '1', '649d3d6c509d4.png', '649d3d6c5084c.png', '2023-06-29 15:14:36', '2023-06-29 15:14:36'),
-(9, 'D.O.T.S. 2022', 'Techno is cordially inviting you to join a thrilling event that we’ve curated wholeheartedly for you guys called “Day of Togetherness and Sharing”! Day of Togetherness and Sharing is an event that will bring all of you together in unity and get to know each other more', 0, '1', '649d3d7e6a764.png', '649d3d7e6a5e7.png', '2023-06-29 15:14:54', '2023-06-29 15:14:54');
+(6, 'Bangkit 2023', 'Aim Higher With Bangkit 2023 merupakan program kerja oleh Himpunan Mahasiswa Program Studi Sistem Informasi (HMPSSI) dan Himpunan Mahasiswa Program Studi Informatika (HMPTIF) UPH Kampus Medan yang bertujuan untuk memperkenalkan program MBKM terkhususnya Bangkit untuk mahasiswa/i UPH Kampus Medan...', 25000, '1', '64a526200885f.png', '64a52d56eec17.png', '2023-06-29 15:13:57', '2023-07-05 15:44:06'),
+(7, 'Menara: Metaverse in Nusantara', 'Menara: Metaverse in Nusantara is an event held by SISTECH to introduce and enrich participants\' knowledge about the technology behind Metaverse and the development of Metaverse in Indonesia...', 35000, '0', '64a5262dc76cf.png', '64a52d6482e11.png', '2023-06-29 15:14:18', '2023-07-05 15:44:20'),
+(8, 'Basic Coding Class', 'Techno has been eavesdropping, hearing rumors that some of you are confused on where to start your first coding journey. Therefore, we’re bringing back “Basic Coding Class” as a platform where you can learn about the fundamentals of coding and sharpen your logical skills...', 125000, '1', '64a526380a191.png', '64a52d6f154e4.png', '2023-06-29 15:14:36', '2023-07-05 15:44:31'),
+(9, 'D.O.T.S. 2022', 'Techno is cordially inviting you to join a thrilling event that we’ve curated wholeheartedly for you guys called “Day of Togetherness and Sharing”! Day of Togetherness and Sharing is an event that will bring all of you together in unity and get to know each other more', 0, '1', '64a52640a120b.png', '64a52d763bd79.png', '2023-06-29 15:14:54', '2023-07-05 15:44:38');
 
 -- --------------------------------------------------------
 
@@ -97,8 +97,8 @@ CREATE TABLE `tbl_admin_home` (
 --
 
 INSERT INTO `tbl_admin_home` (`id`, `name`, `last_modified`, `dataImage`) VALUES
-(1, 'On Going 1', '2023-06-29 11:06:21', '649d033dc257d.png'),
-(2, 'On Going 2', '2023-06-29 11:06:25', '649d0341eae56.png'),
+(1, 'On Going 1', '2023-07-05 15:43:19', '64a52d27987a4.png'),
+(2, 'On Going 2', '2023-07-05 15:42:30', '64a52cf613db9.png'),
 (3, 'On Going 3', '2023-06-29 11:06:29', '649d034529312.png'),
 (4, 'Past Events 1', '2023-06-29 11:06:34', '649d034ad3b7b.png'),
 (5, 'Past Events 2', '2023-06-29 11:06:38', '649d034e5abb2.png'),
@@ -111,7 +111,7 @@ INSERT INTO `tbl_admin_home` (`id`, `name`, `last_modified`, `dataImage`) VALUES
 --
 
 CREATE TABLE `tbl_admin_profile` (
-  `profile_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `profile_title` varchar(40) NOT NULL,
   `profile_description` varchar(40) NOT NULL,
   `profile_image_1` varchar(255) DEFAULT NULL,
@@ -125,15 +125,15 @@ CREATE TABLE `tbl_admin_profile` (
 -- Dumping data for table `tbl_admin_profile`
 --
 
-INSERT INTO `tbl_admin_profile` (`profile_id`, `profile_title`, `profile_description`, `profile_image_1`, `profile_image_2`, `profile_image_3`, `data_created`, `last_modified`) VALUES
-(119, 'governing body', 'hmpssi 2022/2023', '64982559781a4.png', '64982559781a9.png', '64982559781aa.png', '2023-06-25 13:53:28', '2023-06-25 18:30:33'),
-(120, 'internal relation', 'hmpssi 2022/2023', '6497e494ee382.png', '6497e494ee38e.png', '6497e494ee38f.', '2023-06-25 13:54:12', '2023-06-25 13:54:12'),
-(121, 'external relations', 'hmpssi 2022/2023', '6497e63de298d.png', '6497e63de2994.png', '6497e63de2995.', '2023-06-25 14:01:17', '2023-06-25 14:01:17'),
-(123, 'COMMUNICATION MEDIA AND INFORMATION', 'HMPSSI 2022/2023', '6497e70cd1234.png', '6497e70cd1239.png', '6497e70cd123a.png', '2023-06-25 14:04:44', '2023-06-25 14:04:44'),
-(124, 'governing body', 'hmpsif 2022/2023', '6497e75c4672a.png', '6497e75c4672f.png', '6497e75c46730.png', '2023-06-25 14:06:04', '2023-06-25 14:06:04'),
-(125, 'public relation ', 'hmpsif 2022/2023', '6497e7819d690.png', '6497e7819d694.png', '6497e7819d695.png', '2023-06-25 14:06:41', '2023-06-25 14:06:41'),
-(126, 'it development', 'HMPSif 2022/2023', '6497e7d971224.png', '6497e7d97122a.png', '6497e7d97122b.png', '2023-06-25 14:08:09', '2023-06-25 14:08:09'),
-(127, 'COMMUNICATION MEDIA AND INFORMATION', 'HMPSif 2022/2023	', '6497e81181ad4.png', '6497e81181ad8.png', '6497e81181ad9.', '2023-06-25 14:09:05', '2023-06-25 14:09:05');
+INSERT INTO `tbl_admin_profile` (`id`, `profile_title`, `profile_description`, `profile_image_1`, `profile_image_2`, `profile_image_3`, `data_created`, `last_modified`) VALUES
+(119, 'governing body', 'hmpssi 2022/2023', '64a52b8ee66d5.png', '64a52b99792cb.png', '64a52b99ebeaa.png', '2023-06-25 13:53:28', '2023-07-05 15:36:41'),
+(120, 'internal relation', 'hmpssi 2022/2023', '64a52bb0de80a.png', '64a52bb158a95.png', '6497e494ee38f.', '2023-06-25 13:54:12', '2023-07-05 15:37:04'),
+(121, 'external relations', 'hmpssi 2022/2023', '64a52bf82893e.png', '64a52bf89c023.png', '6497e63de2995.', '2023-06-25 14:01:17', '2023-07-05 15:38:16'),
+(123, 'COMMUNICATION MEDIA AND INFORMATION', 'HMPSSI 2022/2023', '64a52c04e4090.png', '64a52c055f104.png', '64a52c05d2937.png', '2023-06-25 14:04:44', '2023-07-05 15:38:28'),
+(124, 'governing body', 'hmpsif 2022/2023', '64a52c10a6122.png', '64a52c1131340.png', '64a52c11a33b0.png', '2023-06-25 14:06:04', '2023-07-05 15:38:40'),
+(125, 'public relation ', 'hmpsif 2022/2023', '64a52c247f1cf.png', '64a52c2507c15.png', '64a52c257f15a.png', '2023-06-25 14:06:41', '2023-07-05 15:39:00'),
+(126, 'it development', 'HMPSif 2022/2023', '64a52c2fcdf18.png', '64a52c305504f.png', '64a52c30d30c0.png', '2023-06-25 14:08:09', '2023-07-05 15:39:11'),
+(127, 'COMMUNICATION MEDIA AND INFORMATION', 'HMPSif 2022/2023	', '64a52c67549c1.png', '64a52c67dd37d.png', '6497e81181ad9.', '2023-06-25 14:09:05', '2023-07-05 15:40:07');
 
 -- --------------------------------------------------------
 
@@ -182,14 +182,7 @@ CREATE TABLE `tbl_akun` (
 --
 
 INSERT INTO `tbl_akun` (`id`, `first_name`, `last_name`, `email`, `password`, `class`, `nim`, `line`, `major`, `intake`, `remember_token`, `verification_code`, `update_verification_time`) VALUES
-(1, 'Ziven', 'Louis', 'ziven123@gmail.com', '11111111', '', '', '', '', '', 'ac927a268636bdc92d4b02a470bf5e91', 'S-742264', NULL),
-(4, 'Erick', 'Thohir', 'erick@gmail.com', '12345678', '', '', '', '', '', '', 'S-432949', '2023-06-05 14:38:35'),
-(5, 'Ziven', 'Louis', 'zivenlouisuph@gmail.com', '12345678', '', '', '', '', '', '198b8ab4c7772c540dd375ae25f6155d', 'S-663111', '2023-06-12 20:43:51'),
-(6, 'Irwanto', 'Wijaya', 'irwantowijaya0@gmail.com', '12345678', '', '', '', '', '', 'eb8ea77f3e911b12307f7a313483c986', 'S-232363', '2023-06-06 18:10:14'),
-(7, 'Jason', 'Tjoa', 'tjoajason2003@gmail.com', '12345678', '', '', '', '', '', '', 'S-848583', '2023-06-22 23:18:09'),
-(8, 'Ziven', 'Louis', 'zivenlouis25@gmail.com', '', '', '', '', '', '', '', 'S-126958', '2023-06-23 19:30:46'),
-(9, 'Darren', 'Oswaldo', 'darrenoswldo@gmail.com', '11111111', '', '', '', '', '', '', 'S-076484', '2023-06-06 18:54:12'),
-(10, 'Erick', 'Ganteng', 'erickleonardo123123@gmail.com', '', '', '', '', '', '', '', 'S-070408', '2023-06-07 17:37:19');
+(15, 'Ziven', 'Louis', 'zivenlouisuph@gmail.com', '12345678', '21TI2', '03082210017', 'zivenlouis25', 'Informatics', '2021', '', NULL, '2023-07-05 17:08:40');
 
 --
 -- Triggers `tbl_akun`
@@ -240,12 +233,6 @@ CREATE TABLE `tbl_events_registration` (
 --
 
 INSERT INTO `tbl_events_registration` (`id`, `event_id`, `account_id`, `payment_status`, `payment_time`) VALUES
-('649e89c146d92', 6, 15, 'Pending', '0000-00-00 00:00:00'),
-('649e8a174bc7e', 6, 15, 'Pending', '0000-00-00 00:00:00'),
-('649e8b1f0a373', 6, 15, 'Pending', '0000-00-00 00:00:00'),
-('649e8e182e593', 6, 15, 'Pending', '0000-00-00 00:00:00'),
-('649e8e46e3df4', 6, 15, 'Pending', '0000-00-00 00:00:00'),
-('649e8e582fb90', 6, 15, 'Pending', '0000-00-00 00:00:00'),
 ('649e8e8ada147', 6, 15, 'Success', '2023-06-30 15:13:09'),
 ('649e929c6cb17', 8, 15, 'Success', '2023-06-30 15:32:29');
 
@@ -275,7 +262,7 @@ ALTER TABLE `tbl_admin_home`
 -- Indexes for table `tbl_admin_profile`
 --
 ALTER TABLE `tbl_admin_profile`
-  ADD PRIMARY KEY (`profile_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_akun`
@@ -316,13 +303,13 @@ ALTER TABLE `tbl_admin_events`
 -- AUTO_INCREMENT for table `tbl_admin_profile`
 --
 ALTER TABLE `tbl_admin_profile`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
 
 --
 -- AUTO_INCREMENT for table `tbl_akun`
 --
 ALTER TABLE `tbl_akun`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tbl_akun_admin`
