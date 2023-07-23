69eec9944afe2e74c7aca0f916f1ed14d6977473
Zivenlouis <zivenlouisuph@gmail.com>
2023-07-23 13:00:29 +0700

    final

--- connection.php
+++ connection.php
@@ -3,6 +3,6 @@
     $username = "root";
     $password = "";  
     $database = "db_web2";
-    $port = "3306";
+    $port = "3307";
     $conn = new mysqli($servername, $username, $password, $database, $port);
 ?>
